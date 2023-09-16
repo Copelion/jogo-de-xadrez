93ea35d4f849d399a02abd43151869bd4c67d5f4
Copelion <copelion@hotmail.com>
2023-09-16 15:35:11 -0400

    captura de erros e limpando tela

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,10 +1,15 @@
 package application;
 
+import java.util.*;
 import boardgame.Board;
 import boardgame.Position;
+import chess.ChessException;
 import chess.ChessMatch;
 import chess.ChessPiece;
 import chess.ChessPosition;
+
+import java.util.InputMismatchException;
+import java.util.Locale;
 import java.util.Scanner;
 
 
@@ -14,21 +19,37 @@ public class Program {
 
     public static void main(String[] args) {
 
+        Locale locale = new Locale("UTF-8");
+
         ChessMatch chessMatch = new ChessMatch();
-        Scanner sc= new Scanner(System.in);
-        while(true) {
+        Scanner sc = new Scanner(System.in);
+        while (true) {
+            try {
 
-            UI.printBoard(chessMatch.getPieces());
-            System.out.println();
-            System.out.print("Source: ");
-            ChessPosition source = UI.readChessPosition(sc);
 
-            System.out.println();
-            System.out.print("Target: ");
-            ChessPosition target = UI.readChessPosition(sc);
+                UI.clearScreen();
+                UI.printBoard(chessMatch.getPieces());
+                System.out.println();
+                System.out.print("Source: ");
+                ChessPosition source = UI.readChessPosition(sc);
 
-            ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
+                System.out.println();
+                System.out.print("Target: ");
+                ChessPosition target = UI.readChessPosition(sc);
 
+                ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
+            }
+            catch(ChessException e){
+                    System.out.println(e.getMessage());
+                    sc.nextLine();
+                }
+            catch(InputMismatchException e){
+                    System.out.println(e.getMessage());
+                    sc.nextLine();
+                }
+
+
+            }
         }
+
     }
-}
